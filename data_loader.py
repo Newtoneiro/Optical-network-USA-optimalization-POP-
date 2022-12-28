24ea3579dd3f6bb31b9690b8d6c8caffa61ad3e6
Szymon Michał Skarzyński <01158894@pw.edu.pl>
2022-12-28 14:58:28 +0100

    Changed into class

--- data_loader.py
+++ data_loader.py
@@ -2,21 +2,35 @@ import xml.etree.ElementTree as ET
 from classes import Link, Demand
 
 
-if __name__ == "__main__":
-    tree = ET.parse("janos-us-ca.xml")
-    root = tree.getroot()
+class DataLoader:
+    def __init__(self, path: str):
+        tree = ET.parse(path)
+        root = tree.getroot()
 
-    nodes = [node.attrib["id"] for node in root[0][0]]
+        nodes = [node.attrib["id"] for node in root[0][0]]
 
-    links = []
-    for link in root[0][1]:
-        source = link[0].text
-        target = link[1].text
-        links.append(Link(source, target))
+        links = []
+        for link in root[0][1]:
+            source = link[0].text
+            target = link[1].text
+            links.append(Link(source, target))
 
-    demands = []
-    for demand in root[1]:
-        source = demand[0].text
-        target = demand[1].text
-        value = float(demand[2].text)
-        demands.append(Demand(source, target, value))
+        demands = []
+        for demand in root[1]:
+            source = demand[0].text
+            target = demand[1].text
+            value = float(demand[2].text)
+            demands.append(Demand(source, target, value))\
+
+        self._nodes = nodes
+        self._links = links
+        self._demands = demands
+
+    def get_nodes(self):
+        return self._nodes
+
+    def get_links(self):
+        return self._links
+
+    def get_demands(self):
+        return self._demands
