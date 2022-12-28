5ef66add6727d0743d01fab2b57eab2a65bc0fb8
Szymon Michał Skarzyński <01158894@pw.edu.pl>
2022-12-28 14:37:51 +0100

    moved classes to other file

--- data_loader.py
+++ data_loader.py
@@ -1,17 +1,5 @@
 import xml.etree.ElementTree as ET
-
-
-class Link:
-    def __init__(self, source: str, target: str) -> None:
-        self.source = source
-        self.target = target
-
-
-class Demand:
-    def __init__(self, source: str, target: str, value: float) -> None:
-        self.source = source
-        self.target = target
-        self.value = value
+from classes import Link, Demand
 
 
 if __name__ == "__main__":
