3fdb6bb9a30bfc064db14f9af4a6b617a4446afb
Szymon Michał Skarzyński <01158894@pw.edu.pl>
2022-12-28 14:58:00 +0100

    Replace classes.py

--- classes.py
+++ classes.py
@@ -3,9 +3,15 @@ class Link:
         self.source = source
         self.target = target
 
+    def __str__(self) -> str:
+        return f"{self.source} - {self.target}"
+
 
 class Demand:
     def __init__(self, source: str, target: str, value: float) -> None:
         self.source = source
         self.target = target
         self.value = value
+
+    def __str__(self) -> str:
+        return f"{self.source} - {self.target} - {self.value}"
