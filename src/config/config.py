11b7e0c29bd17a56238679e8c919868f38eb424d
sskarzyn <01158894@pw.edu.pl>
2023-01-21 20:14:43 +0100

    cost calc upgrade

--- src/config/config.py
+++ src/config/config.py
@@ -5,9 +5,14 @@ MAX_PATH_CAPACITY = 1008
 TRANSPONDERS = [100, 200, 400]
 
 # tranponders costs
-COST_100G = 1
-COST_200G = 3
-COST_400G = 7
+TRANSPONDERS_COSTS = {
+    100: 1,
+    200: 3,
+    400: 7
+}
 
 # lambda cost
 COST_LAMBDA = 5
+
+# mutation
+DEMAND_MUTATION_PROBABILITY = 0.2
